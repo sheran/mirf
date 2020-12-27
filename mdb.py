6b2a7540feec5f9fc5f09318bc1ddc8e891f3fbf
Sheran Gunasekera <sheran@zensay.com>
2020-12-28 01:30:13 +0800

    Finished iOS SMS DB Parsing - Gross code

--- mdb.py
+++ mdb.py
@@ -115,7 +115,7 @@ class SqliteDB:
                 old_num = num[0]
                 continue
             if num[0] - old_num > 1:
-                indexes.add(idx)
+                indexes.add(idx+1)
             old_num = num[0]
         return indexes
 
@@ -134,54 +134,92 @@ class SqliteDB:
         c.execute("select seq from sqlite_sequence where name='message'")
         num_rows = c.fetchone()
         num_deleted = num_rows[0] - max_rowid[0]
-        print(f"Latest Row ID: {num_rows[0]}")
-        print(f"Number of Rows found: {max_rowid[0]}")
+        print(f"Latest Row ID in 'message' table: {num_rows[0]}")
+        print(f"Number of Rows found in 'message' table: {max_rowid[0]}")
         deleted_rows = set()
         for r in range(num_deleted):
             deleted_rows.add(max_rowid[0] + r + 1)
         
         # Now we check the rest of the message table for missing rows
-        gaps = self.findGaps('message','rowid')
+        gaps = self.findGaps('message','ROWID')
         if len(gaps) > 0:
             for gap in gaps:
                 deleted_rows.add(gap)
         
         # See if we have gaps or missing rows
         if len(deleted_rows) > 0:
-            print(sorted(deleted_rows))
+            # Build the gaps section
+            # Iterate over the deleted rows getting records before and
+            # after the deleted row
+            active_rows = set()
+            for row in sorted(deleted_rows):
+                row_before = row - 1
+                while row_before in deleted_rows:
+                    row_before -= 1
+                if row_before < 0:
+                    row_before = None
+                row_after  = row + 1
+                while row_after in deleted_rows:
+                    row_after += 1
+                if row_after > num_rows[0]:
+                    row_after = None
+                if row_before is not None:
+                    active_rows.add(row_before)
+                if row_after is not None:
+                    active_rows.add(row_after)
+            
+            selstr = "("
+            for k,v in enumerate(active_rows):
+                selstr += f"'{v}'"
+                if k < len(active_rows) - 1:
+                    selstr += ", "
+            selstr += ")"
+
+            # fetch data from active rows
+            c.execute(f"select ROWID, date_delivered, date_read, text from"\
+                f" message where ROWID in {selstr}")
+            flat_rows = set()
+            for row in c.fetchall():
+                dd = time.asctime(time.gmtime( (row[1] / 1000000000) + \
+                    978307200 )) if row[1] > 0 else "Not Set"
+                dr = time.asctime(time.gmtime( (row[2] / 1000000000) + \
+                    978307200 )) if row[2] > 0 else "Not Set"
+                flat_rows.add((row[0],dd,dr,row[3].replace("\n","")))
+            for row in deleted_rows:
+                flat_rows.add((row,"-- missing --","-- missing --",\
+                    "-- missing --"))
+            self.printGaps(flat_rows)
+            
         else:
-            print("No missing records found")    
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # if num_deleted  > 0:
-        #     # The last (num_rows[0] - max_rowid[0]) rows of the message table 
-        #     # has been deleted
-        #     print(f"{num_deleted} row(s) have been deleted from the 'message'"\
-        #         " table")
-        #     # Find the timestamp of the last available row
-        #     c.execute(f"select rowid, date_delivered, date_read from message "\
-        #         f"where rowid={max_rowid[0]}")
-        #     t = c.fetchone()
-        #     # Mac Core Data time stamp is from year 1904 so we have to add 
-        #     # 978307200 seconds to it to bring it to the Unix epoch of 1970
-        #     dd = time.asctime(time.gmtime( (t[1] / 1000000000) + 978307200 )) \
-        #         if t[1] > 0 else "Not Set"
-        #     dr = time.asctime(time.gmtime( (t[2] / 1000000000) + 978307200 )) \
-        #         if t[2] > 0 else "Not Set"
-        
+            print("No missing records found in 'message' table")
         
 
+    #Print out the active and missing rows
+    def printGaps(self, flat_rows):
+        # Print the table out
+        # Col size is: 10, 25, 25, 40 (truncate)
+        # hardcoded headers: ROWID, date_delivered, date_received, text
+        print(f"+{'':->10}+{'':->25}+{'':->25}+{'':->40}+")
+        # print headers
+        print(f'|{"ROWID":>10}|{"date_delivered":>25}|{"date_read":>25}|'\
+            f'{"text":>40}|')
+        print(f"+{'':->10}+{'':->25}+{'':->25}+{'':->40}+")
+        # print the data rows
+        fl_rows = sorted(flat_rows)
 
-
-
+        for row in fl_rows:
+            try:
+                old_num
+            except NameError:
+                old_num = row[0]
+            if row[0] - old_num > 1:
+                print(f'|{".":>10}|{".":>25}|{".":>25}|{".":>40.40}|')
+                print(f'|{".":>10}|{".":>25}|{".":>25}|{".":>40.40}|')
+                print(f'|{".":>10}|{".":>25}|{".":>25}|{".":>40.40}|')
+            print(f'|{row[0]:>10}|{row[1]:>25}|{row[2]:>25}|{row[3]:>40.40}|')
+            old_num = row[0]
+        print(f"+{'':->10}+{'':->25}+{'':->25}+{'':->40}+")
+        
     def close(self):
         self.conn.close()
 
